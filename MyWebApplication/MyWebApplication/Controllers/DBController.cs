39464d757cf13449ca540ca5b2a2daf638ffd719
Hemachandran Harimoorthy <hemachandranhj@hotmail.com>
2016-06-07 23:03:48 +0530

    WCF Service Cnsumption

---
+++ MyWebApplication/MyWebApplication/Controllers/DBController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebApplication.Controllers
+{
+    public class DBController : Controller
+    {
+        // GET: DB
+        public ActionResult Index()
+        {
+            ServiceReference1.ServiceClient sc = new ServiceReference1.ServiceClient();
+            return View(sc.GetData());
+        }
+    }
+}
